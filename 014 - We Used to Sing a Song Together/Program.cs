e970e8ce22fe3a122a5e70c885d33ab14992b09e
keyam <kei.329.yama@icloud.com>
2022-04-24 19:43:11 +0900

    20220424

---
+++ 014 - We Used to Sing a Song Together/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Program {
+    public static void Main(string[] args) {
+        int n = int.Parse(Console.ReadLine());
+        var orderedA = Console.ReadLine().Split(" ").Select(int.Parse).OrderBy(x => x).ToArray();
+        var orderedB = Console.ReadLine().Split(" ").Select(int.Parse).OrderBy(x => x).ToArray();
+
+        long result = 0;
+        for (int i = 0; i < n; i++) { 
+            result += Math.Abs(orderedA[i] - orderedB[i]);
+        }
+
+        Console.WriteLine(result);
+    }
+}
