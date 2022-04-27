7d6379e5caa77a1c8e2c79ac124b4ea271c35912
keyam <kei.329.yama@icloud.com>
2022-04-27 22:37:14 +0900

    20220427_3

---
+++ 033 - Not Too Bright（★2）/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Program {
+    public static void Main(string[] args) {
+        var hw = Console.ReadLine().Split(" ");
+        int h = int.Parse(hw[0]);
+        int w = int.Parse(hw[1]);
+
+        if (h == 1 || w == 1) {
+            Console.WriteLine(h * w);
+        } else {
+            decimal temp1 = Math.Ceiling(h / 2.0M);
+            decimal temp2 = Math.Ceiling(w / 2.0M);
+
+            Console.WriteLine(temp1 * temp2);
+        }
+
+    }
+}
