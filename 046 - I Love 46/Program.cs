4dce50a54e04687070184195fd9ad9e20670b315
keyam <kei.329.yama@icloud.com>
2022-04-30 20:05:50 +0900

    20220430

---
+++ 046 - I Love 46/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Program {
+
+
+    public static void Main(string[] args) {
+        var n = int.Parse(Console.ReadLine());
+        var modAAndCnt = Console.ReadLine().Split(" ").Select(x => int.Parse(x) % 46).GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        var modBAndCnt = Console.ReadLine().Split(" ").Select(x => int.Parse(x) % 46).GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        var modCAndCnt = Console.ReadLine().Split(" ").Select(x => int.Parse(x) % 46).GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+        long cnt = 0;
+        foreach (var modA in modAAndCnt.Keys) {
+            foreach (var modB in modBAndCnt.Keys) {
+                foreach (var modC in modCAndCnt.Keys) {
+                    if ((modA + modB + modC) % 46 == 0) {
+                        cnt += (long)modAAndCnt[modA] * (long)modBAndCnt[modB] * (long)modCAndCnt[modC];
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(cnt);
+    }
+}
--- README.md
+++ README.md
@@ -18,3 +18,7 @@
   - [027 - Sign Up Requests （★2）](https://atcoder.jp/contests/typical90/tasks/typical90_aa)
 - 20220428
   - [038 - Large LCM（★3）](https://atcoder.jp/contests/typical90/tasks/typical90_al)
+- 20220429
+  - [044 - Shift and Swapping（★3）](https://atcoder.jp/contests/typical90/tasks/typical90_ar)
+- 20220430
+  - [046 - I Love 46（★3）](https://atcoder.jp/contests/typical90/tasks/typical90_at)
